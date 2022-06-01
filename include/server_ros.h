dd20dd13a72a0b08d1da27002a22d531b6fd9b9b
matthewoots <matthewoots@gmail.com>
2022-06-01 21:58:08 +0800

    [librrtserver] Fixed bugs in the rrt modules, and did a first cut in fixing the transformation, check validity using a safe corridor [server_ros] completed the integration of the module with ros and added mockamap for testing and visualization

--- include/server_ros.h
+++ include/server_ros.h
@@ -1,5 +1,5 @@
 /*
-* trajectory_server_ros.h
+* server_ros.h
 *
 * ---------------------------------------------------------------------
 * Copyright (C) 2022 Matthew (matthewoots at gmail.com)
@@ -15,8 +15,8 @@
 *  GNU General Public License for more details.
 * ---------------------------------------------------------------------
 */
-#ifndef TRAJECTORY_SERVER_ROS_H
-#define TRAJECTORY_SERVER_ROS_H
+#ifndef SERVER_ROS_H
+#define SERVER_ROS_H
 
 #include "rrt_server_module.h"
 
@@ -28,14 +28,22 @@
 #include <random>
 #include <Eigen/Dense>
 #include <ros/ros.h>
-#include <random>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/conversions.h>
 
 #include <nav_msgs/Path.h>
 
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Point.h>
 
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
 #include <tf/tf.h>
+
+#include "rrt_server_module.h"
 
 #define KNRM  "\033[0m"
 #define KRED  "\033[31m"
@@ -48,24 +56,154 @@
 
 using namespace Eigen;
 using namespace std;
+using namespace rrt_server;
 
 class rrt_server_ros
 {
     private:
 
+        std::mutex search_points_mutex, pose_update_mutex, cloud_mutex;
+
         ros::NodeHandle _nh;
+        ros::Subscriber pcl2_msg_sub;
+        ros::Publisher local_pcl_pub, l_rrt_points_pub, g_rrt_points_pub, debug_pcl_pub;
+
+        double _runtime_error, _sub_runtime_error, _search_interval;
+        double min_height, max_height;
+        double search_radius, obs_threshold;
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr full_cloud;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr local_cloud;
+
+        Eigen::Vector3d start, end, current_control_point;
+        Eigen::Vector3d _local_map_size_xyz;
+        Eigen::Vector3d direct_goal;
+
+        vector<Eigen::Vector3d> previous_search_points;
+        vector<Eigen::Vector3d> global_search_path;
+
+        vector<Eigen::Vector4d> no_fly_zone;
+
+        ros::Time last_pcl_msg;
+
+        rrt_server::rrt_utility ru; 
+        rrt_server::rrt_server_node fe_rrt_server;
+
+        ros::Timer search_timer;
+
+        void pcl2_callback(const sensor_msgs::PointCloud2ConstPtr& msg);
+    
+        void extract_direct_goal(pcl::PointCloud<pcl::PointXYZ>::Ptr obs);
+
+        void check_and_update_search(pcl::PointCloud<pcl::PointXYZ>::Ptr obs);
+
+        void generate_search_path(pcl::PointCloud<pcl::PointXYZ>::Ptr obs_pcl);
+
+        void run_search_timer(const ros::TimerEvent &);
 
     public:
 
         rrt_server_ros(ros::NodeHandle &nodeHandle) : _nh(nodeHandle)
         {
+            /** @brief ROS Params */
+            _nh.param<double>("runtime_error", _runtime_error, 0.1);
+            _nh.param<double>("sub_runtime_error", _sub_runtime_error, 0.02);  
+            _nh.param<double>("search_radius", search_radius, 5.0);
+            _nh.param<double>("threshold", obs_threshold, 0.5); 
+            _nh.param<double>("search_interval", _search_interval, 0.5); 
 
+            std::vector<double> start_list, end_list, local_map_size_list, height_list;
+
+            _nh.getParam("start_position", start_list);
+            _nh.getParam("end_position", end_list);
+            _nh.getParam("local_map_size_xyz", local_map_size_list);
+            _nh.getParam("height", height_list);
+
+            for(int i = 0; i < (int)local_map_size_list.size(); i++) 
+                _local_map_size_xyz[i] = local_map_size_list[i];
+ 
+            min_height = height_list[0];
+            max_height = height_list[1];
+
+            start = Eigen::Vector3d(start_list[0], start_list[1], start_list[2]);
+            end = Eigen::Vector3d(end_list[0], end_list[1], end_list[2]);
+
+            std::cout << "start and end positions \n[" << start.transpose() <<
+                 "] [" << end.transpose() << "]" << std::endl;
+
+            pcl2_msg_sub = _nh.subscribe<sensor_msgs::PointCloud2>(
+                "/mock_map", 1,  boost::bind(&rrt_server_ros::pcl2_callback, this, _1));
+
+            /** @brief For debug */
+            local_pcl_pub = _nh.advertise<sensor_msgs::PointCloud2>("/local_map", 10);
+            l_rrt_points_pub = _nh.advertise<nav_msgs::Path>("/rrt_points_local", 10);
+            g_rrt_points_pub = _nh.advertise<nav_msgs::Path>("/rrt_points_global", 10);
+            debug_pcl_pub = _nh.advertise<sensor_msgs::PointCloud2>("/debug_map", 10);
+
+            /** @brief Timer that when to search */
+		    search_timer = _nh.createTimer(
+                ros::Duration(_search_interval), 
+                &rrt_server_ros::run_search_timer, this, false, false);
+
+            current_control_point = start;
+            search_timer.start();
         }
 
 
         ~rrt_server_ros()
         {
+            search_timer.stop();
+        }
 
+        /** @brief Convert point cloud from ROS sensor message to pcl point ptr **/
+        pcl::PointCloud<pcl::PointXYZ>::Ptr 
+            pcl2_converter(sensor_msgs::PointCloud2 _pc)
+        {
+            pcl::PCLPointCloud2 pcl_pc2;
+            pcl_conversions::toPCL(_pc, pcl_pc2);
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+            
+            pcl::fromPCLPointCloud2(pcl_pc2, *tmp_cloud);
+            
+            return tmp_cloud;
+        }
+
+        bool inside_sphere_check(Eigen::Vector3d point, 
+            Eigen::Vector3d sphere_center, double radii)
+        {
+            // get the distance vector
+            Eigen::Vector3d dv = sphere_center - point;
+            double a = 1.0; 
+            double b = 1.0; 
+            double inv_a2 = 1 / a / a; 
+            double inv_b2 = 1 / b / b; 
+            // get the ellipsoidal distance 
+            double e_d = sqrt(pow(dv.z(),2) * inv_a2 + 
+                (pow(dv.x(),2) + pow(dv.y(),2)) * inv_b2); 
+            // get the distance error
+            double d_e = radii - e_d; 
+            if (d_e > 0)
+                return true;
+            else 
+                return false;
+        }
+
+        nav_msgs::Path vector_3d_to_path(vector<Vector3d> path_vector)
+        {
+            nav_msgs::Path path;
+            path.header.stamp = ros::Time::now();
+            path.header.frame_id = "world";
+            for (int i = 0; i < path_vector.size(); i++)
+            {
+                geometry_msgs::PoseStamped pose;
+                pose.pose.position.x = path_vector[i][0];
+                pose.pose.position.y = path_vector[i][1];
+                pose.pose.position.z = path_vector[i][2];
+                path.poses.push_back(pose);
+            }
+
+            return path;
         }
         
 };
--- src/server_ros.cpp
+++ src/server_ros.cpp
@@ -27,6 +27,190 @@
 #define KCYN  "\033[36m"
 #define KWHT  "\033[37m"
 
-using namespace Eigen;
 using namespace std;
+using namespace Eigen;
+using namespace rrt_server;
 
+void rrt_server_ros::pcl2_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
+{
+    full_cloud = pcl2_converter(*msg);
+    std::lock_guard<std::mutex> cloud_lock(cloud_mutex);
+    local_cloud = ru.pcl_ptr_box_crop(full_cloud, start, _local_map_size_xyz);
+    last_pcl_msg = ros::Time::now();
+    local_pcl_pub.publish(local_cloud);
+    return;
+}
+
+void rrt_server_ros::extract_direct_goal(pcl::PointCloud<pcl::PointXYZ>::Ptr obs)
+{
+    double radii;
+    // If end goal is inside the search sphere
+    if ((end - current_control_point).norm() < search_radius)
+        radii = (end - current_control_point).norm();
+    // If end goal is outside the search sphere
+    else
+        radii = search_radius;
+
+    // Find a RRT path that is quick and stretches to the end point
+    vector<Eigen::Vector3d> path = fe_rrt_server.find_rrt_path(
+        previous_search_points, obs, current_control_point, end, radii);
+    // If path gives an invalid value, execute some form of emergency
+    if (path.empty())
+    {
+        return;
+    }
+
+    // Save global_path
+    global_search_path = path;
+
+    // Find the intersection between any of the legs and the sphere perimeter
+    int failed_counts = 0;
+    int intersection_idx;
+    for (int i = 1; i < (int)path.size(); i++)
+    {
+        // outside : previous_search_points[i]
+        // inside : previous_search_points[i-1]
+        if (inside_sphere_check(path[i-1], current_control_point, search_radius) &&
+            !inside_sphere_check(path[i], current_control_point, search_radius))
+        {
+            intersection_idx = i;
+            break;
+        }
+        failed_counts++;
+    }
+
+    // We have reached the goal if failed counts is same as path size
+    if (failed_counts == path.size()-1)
+    {
+        direct_goal = end;
+        return;
+    }
+
+    // Calculate the direct goal point from the intersection pair
+    // Find the intersection with the sphere
+    Eigen::Vector3d vect1 = path[intersection_idx] - path[intersection_idx-1];
+    double vect1_norm = vect1.norm();
+    Eigen::Vector3d vect2 = current_control_point - path[intersection_idx-1];
+    double vect2_norm = vect2.norm();
+    double dot = vect1.x()*vect2.x() + vect1.y()*vect2.y() + vect1.z()*vect2.z();
+    double extension = dot / vect1_norm;
+    double direct_distance_from_start;
+    if (extension <= 0.00001 && extension >= -0.00001)
+    {
+        double nearest_dist = sqrt(pow(vect2_norm,2) - pow(extension,2));
+        double sub_distance = sqrt(pow(radii,2) - pow(nearest_dist,2)); 
+        direct_distance_from_start = sub_distance + extension;
+    }
+    else
+    {
+        direct_distance_from_start = sqrt(pow(radii,2) - pow(extension,2)); 
+    }
+
+    direct_goal = direct_distance_from_start * (vect1 / vect1_norm);
+    return;
+}
+
+void rrt_server_ros::generate_search_path(
+    pcl::PointCloud<pcl::PointXYZ>::Ptr obs_pcl)
+{
+    // check previous search points and create an updated one
+    // We moved forward, so find the closes point from current pose
+    check_and_update_search(obs_pcl);
+
+    vector<Eigen::Vector3d> path = fe_rrt_server.find_rrt_path(
+        previous_search_points, obs_pcl, current_control_point, 
+        direct_goal, search_radius / 2);
+
+    std::lock_guard<std::mutex> search_points_lock(search_points_mutex);
+    if (path.empty())
+        return;
+    
+    previous_search_points.clear();
+    previous_search_points = path;
+
+    return;
+}
+
+void rrt_server_ros::check_and_update_search(
+    pcl::PointCloud<pcl::PointXYZ>::Ptr obs)
+{
+    std::lock_guard<std::mutex> pose_lock(pose_update_mutex);
+    std::lock_guard<std::mutex> search_points_lock(search_points_mutex);
+
+    if (previous_search_points.empty())
+        return;
+
+    int last_safe_idx = -1;
+    int initial_size = (int)previous_search_points.size();
+    for (int i = 1; i < initial_size; i++)
+    {
+        if (!ru.check_line_validity_with_pcl(
+            previous_search_points[i], previous_search_points[i-1], obs_threshold, obs))
+        {
+            last_safe_idx = i-1;
+            break;
+        }
+    }
+
+    if (last_safe_idx >= 0)
+    {
+        for (int i = last_safe_idx + 1; i < initial_size; i++)
+            previous_search_points.erase(previous_search_points.end());
+    }
+
+    int new_size = (int)previous_search_points.size();
+    double nearest_distance = DBL_MAX;
+    int idx = -1; 
+    for (int i = 0; i < new_size; i++)
+    {
+        double distance = (previous_search_points[i]-current_control_point).norm();
+        if (distance < nearest_distance)
+        {
+            nearest_distance = distance;
+            idx = i;
+        }
+    }
+
+    for (int i = idx-1; i >= 0; i--)
+        previous_search_points.erase(previous_search_points.begin());
+}
+
+void rrt_server_ros::run_search_timer(const ros::TimerEvent &)
+{
+    if ((ros::Time::now() - last_pcl_msg).toSec() > 2.0)
+        return;
+
+    std::lock_guard<std::mutex> cloud_lock(cloud_mutex);
+
+    fe_rrt_server.reset_parameters(
+        vector<Eigen::Vector4d>(),
+        min_height, max_height, obs_threshold,
+        _sub_runtime_error, _runtime_error);
+
+    ros::Time global_start_time = ros::Time::now();
+    extract_direct_goal(local_cloud);
+    ros::Duration global_time = ros::Time::now() - global_start_time;
+
+    // ros::Time local_start_time = ros::Time::now();
+    // generate_search_path(local_cloud);
+    // ros::Duration local_time = ros::Time::now() - local_start_time;
+
+    nav_msgs::Path local_path = vector_3d_to_path(previous_search_points);
+    nav_msgs::Path global_path = vector_3d_to_path(global_search_path);
+
+    std::cout << "[server_ros] global_time " << 
+        KGRN << global_time.toSec() << KNRM << "s" << std::endl;
+    // std::cout << "[server_ros] local_time " <<
+    //     KGRN << local_time.toSec()  << KNRM << "s" << std::endl;
+
+    sensor_msgs::PointCloud2 object_msg;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr object_cloud;
+    object_cloud = fe_rrt_server.get_debug_local_pcl();
+    pcl::toROSMsg(*object_cloud.get(), object_msg);
+    object_msg.header.frame_id = "world";
+    
+
+    l_rrt_points_pub.publish(local_path);
+    g_rrt_points_pub.publish(global_path);
+    debug_pcl_pub.publish(object_msg);
+}
